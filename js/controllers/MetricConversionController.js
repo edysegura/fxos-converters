9a785dcfd2859bb643015c154acd9413f0eceeee
FlavioFerreira <flavinhoapf@hotmail.com>
2013-10-01 20:23:46 -0300

    verificar alterações

---
+++ js/controllers/MetricConversionController.js
@@ -0,0 +1,110 @@
+conversionApp.controller('MetricConversionController', function($scope) {
+
+	//initialization	
+	$scope.isKmToMile = true;
+	$scope.$on('$viewContentLoaded', function() {
+		$scope.resetFields();
+		$scope.setCurrent();
+	});
+	
+	$scope.kmToMile = function() {
+		if($scope.km) {
+			$scope.mile = $scope.km * 0.621371192;
+		}
+		else {
+			$scope.mile = '';
+		}
+	};
+
+	$scope.mileToKm = function() {
+		if($scope.mile) {
+			$scope.km = $scope.mile * 1.621371192;
+		}
+		else {
+			$scope.mile = '';
+		}
+	};
+
+	$scope.celsiusToFahrenheit = function() {
+		if($scope.celsius) {
+			$scope.fahrenheit = $scope.celsius * 1.8 + 32;
+		}
+		else {
+			$scope.celsius = '';
+		}
+	};
+
+	$scope.fahrenheitToCelsius = function() {
+		if($scope.fahrenheit) {
+			$scope.celsius = ($scope.fahrenheit - 32) * (5/9);
+		}
+		else {
+			$scope.celsius = '';
+		}
+	};
+
+	$scope.kelvinToCelsius = function() {
+		if($scope.kelvin) {
+			$scope.celsius = $scope.kelvin - 273;
+		}
+		else {
+			$scope.celsius = '';
+		}
+	};
+
+	$scope.CelsiusToKelvin = function() {
+		if($scope.celsius) {
+			$scope.kelvin = $scope.celsius + 273;
+		}
+		else {
+			$scope.kelvin = '';
+		}
+	};
+
+	$scope.setKmToMile = function() {
+		$scope.isKmToMile = $scope.setCurrent();
+	};
+
+	$scope.setMileToKm = function() {
+		$scope.isMileToKm = $scope.setCurrent();
+	};
+
+	$scope.setCelsiusToFahrenheit = function() {
+		$scope.isCelsiusToFahrenheit = $scope.setCurrent();
+	};
+
+	$scope.setFahrenheitToCelsius = function() {
+		$scope.isFahrenheitToCelsius = $scope.setCurrent();
+	};
+
+	$scope.setKelvinToCelsius = function() {
+		$scope.isKelvinToCelsius = $scope.setCurrent();
+	};
+
+	$scope.setCelsiusToKelvin = function() {
+		$scope.isCelsiusToKelvin = $scope.setCurrent();
+	};
+
+	$scope.resetFields = function() {
+		$scope.km = '';
+		$scope.mile = '';
+		$scope.celsius = '';
+		$scope.fahrenheit = '';
+		$scope.kelvin = '';
+
+	};
+
+	$scope.setCurrent = function() {
+		$scope.resetFields();
+
+		$scope.isMileToKm =  false;
+		$scope.isKmToMile =  false;
+		$scope.isCelsiusToFahrenheit =  false;
+		$scope.isFahrenheitToCelsius =  false;
+		$scope.isKelvinToCelsius =  false;
+		$scope.isCelsiusToKelvin =  false;
+
+		return true;
+	};
+
+});
