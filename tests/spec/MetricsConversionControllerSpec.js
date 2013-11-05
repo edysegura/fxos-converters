593773cac615ce2415c3a495c97d4fd0db135db5
Edy Segura <edysegura@gmail.com>
2013-11-05 20:46:05 -0200

    Update MetricsConversionControllerSpec.js
    Increasing the test coverage

--- tests/spec/MetricsConversionControllerSpec.js
+++ tests/spec/MetricsConversionControllerSpec.js
@@ -21,25 +21,100 @@ describe("MetricConversionController", function() {
 		expect(scope.setCurrent).toHaveBeenCalled();
 	});
 
-	it('should convert 1 kilomiter to mile', function() {
-		//given
-		scope.km = 1;
+	describe('Length', function() {
+		it('should convert 1 kilomiter to 0.621371192 mile', function() {
+			//given
+			scope.km = 1;
 
-		//when
-		scope.kmToMile();
+			//when
+			scope.kmToMile();
 
-		//then
-		expect(scope.mile).toBe(0.621371192);
+			//then
+			expect(scope.mile).toBe(0.621371192);
+		});
+
+		it('should convert 1 mile to kilometer 1.621371192', function() {
+			//given
+			scope.mile = 1;
+
+			//when
+			scope.mileToKm();
+
+			//then
+			expect(scope.km).toBe(1.621371192);
+		});
 	});
 
-	it('should convert 1 mile to kilometer', function() {
-		//given
-		scope.mile = 1;
+	describe('Temperature', function() {
+		it('should convert 1 celsius to 33.8 fahrenheit', function() {
+			//given
+			scope.celsius = 1;
 
-		//when
-		scope.mileToKm();
+			//when
+			scope.celsiusToFahrenheit();
 
-		//then
-		expect(scope.km).toBe(1.621371192);
+			//then
+			expect(scope.fahrenheit).toBe(33.8);
+		});
+
+		it('should convert 1 celsius to 274.15 kelvin', function() {
+			//given
+			scope.celsius = 1;
+
+			//when
+			scope.celsiusToKelvin();
+
+			//then
+			expect(scope.kelvin).toBe(274.15);
+		});
+
+		it('should convert 1 fahrenheit to -17.22222222222222 celsius', function() {
+			//given
+			scope.fahrenheit = 1;
+
+			//when
+			scope.fahrenheitToCelsius();
+
+			//then
+			expect(scope.celsius).toBe(-17.22222222222222);
+		});
+
+		it('should convert 1 fahrenheit to -255.928 kelvin', function() {
+			//given
+			scope.fahrenheit = 1;
+
+			//when
+			scope.fahrenheitToKelvin();
+
+			//then
+			expect(scope.kelvin).toBe(255.9277777777778);
+		});
+
+		it('should convert 1 kelvin to -272.15 celsius', function() {
+			//given
+			scope.kelvin = 1;
+
+			//when
+			scope.kelvinToCelsius();
+
+			//then
+			expect(scope.celsius).toBe(-272.15);
+		});
+
+		xit('should convert 1 kelvin to -457.87 fahrenheit', function() {
+			//given
+			scope.kelvin = 1;
+
+			/*
+			Kelvin To Fahrenheit: 
+			((Kelvin - 273) * 1.8 ) + 32; note: 1.8 = 9/5
+			*/
+			//when
+			scope.kelvinToFahrenheit();
+
+			//then
+			expect(scope.fahrenheit).toBe(-457.87);
+		});
 	});
+
 });
